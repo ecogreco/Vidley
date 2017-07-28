1c55b82c50a737c61f2c325d48eede173f801765
Daniel Greco <ecogreco@yahoo.com>
2017-07-28 15:35:34 -0400

    Added in the New Customer View

--- Vidley/Controllers/CustomersController.cs
+++ Vidley/Controllers/CustomersController.cs
@@ -24,6 +24,16 @@ namespace Vidley.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
+        }
+
 
         public ActionResult Index()
         {
