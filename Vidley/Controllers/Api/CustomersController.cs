b9b5b1a9e59a205a23c3292c3ac2810708ce20f2
Daniel Greco <ecogreco@yahoo.com>
2017-07-31 18:08:13 -0400

    Started to use API forms

---
+++ Vidley/Controllers/Api/CustomersController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidley.Models;
+
+namespace Vidley.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET/API/Customers 
+        public IEnumerable<Customer> GetCustomers() //returns list of customers
+        {
+            return _context.Customers.ToList();
+        }
+
+        //GET /api/customers/1
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id); // in case the customer is null
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        //POST /api / customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if(!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        //PUT /customer /1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customerInDB == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            customerInDB.Name = customer.Name;
+            customerInDB.Birthday = customer.Birthday;
+            customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDB.MembershipTypeId = customer.MembershipTypeId;
+            _context.SaveChanges();
+
+        }
+
+        //DELETE /Customer/api /1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDB == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customers.Remove(customerInDB);
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ Vidley/Dtos/CustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidley.Models;
+
+namespace Vidley.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]//property where Name can not be null
+        [StringLength(255)]//property that sets the minimum length to 255
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; 
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember] //this class is created and uses the interface for validation attribute to validate if the date of birth is 18 years or older to be a member
+        public DateTime? Birthday { get; set; }
+    }
+}
