c20c9f252dc690a614a28d3bf72c9bf5cc9a89cd
Daniel Greco <ecogreco@yahoo.com>
2017-08-10 17:57:15 -0400

    trying to add in return form

--- Vidley/Controllers/RentalsController.cs
+++ Vidley/Controllers/RentalsController.cs
@@ -1,17 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using Vidley.Models;
+using Vidley.ViewModels;
 
 namespace Vidley.Controllers
 {
     public class RentalsController : Controller
     {
-        
+        public ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult New()
         {
             return View();
         }
+
+        public ActionResult Return()
+        {
+            var viewModel = new ReturnMovieViewModel();
+            return View(viewModel);
+        }
+
+       
+        [ValidateAntiForgeryToken]
+        public ActionResult Returned(ReturnMovieViewModel movie)
+        {
+            if(!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                
+            }
+
+            
+
+            try
+            {
+                var movieInDB = _context.Movies.Single(m => m.Name == movie.Name);
+                if (movieInDB.NumberAvailable >= movieInDB.NumberInStock)
+                {
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);
+                }
+                movieInDB.NumberAvailable++;
+                _context.SaveChanges();
+            }
+            
+
+            catch(Exception e)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            
+           
+
+            return RedirectToAction("Return", "Rentals");
+        }
     }
+
 }
---
+++ Vidley/ViewModels/ReturnMovieViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidley.Models;
+
+namespace Vidley.ViewModels
+{
+    public class ReturnMovieViewModel
+    {
+        public int Id;
+        [Required]
+        public string Name;
+
+        public ReturnMovieViewModel()
+        {
+
+        }
+
+        public ReturnMovieViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+        }
+    }
+}
